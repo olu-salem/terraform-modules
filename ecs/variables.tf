2c2e165ee9b76a6dd6b208554c7e99d286ff56d9
Olusegun <olusegun.salem@gmail.com>
2024-06-20 05:28:37 -0500

    fixed the errors in variables files on line 10 and 11

--- ecs/variables.tf
+++ ecs/variables.tf
@@ -7,7 +7,7 @@ variable "container_image" {}
 variable "env_file_bucket_name" {}
 variable "env_file_name" {}
 variable "region" {}
-variable "private_app_subnet_az1.id" {}
-variable "private_app_subnet_az2.id" {}
+variable "private_app_subnet_az1_id" {}
+variable "private_app_subnet_az2_id" {}
 variable "app_server_security_group_id" {}
 variable "alb_target_group_arn" {}
